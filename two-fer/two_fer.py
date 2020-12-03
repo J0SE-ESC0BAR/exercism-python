05eaafde99041f8c29865d8dd2df138046aedf6d
jaem-rgb <74581066+jaem-rgb@users.noreply.github.com>
2020-12-02 19:09:28 -0600

    solucion 1

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,10 @@
 def two_fer(name):
-    pass
+
+
+    nombre = input("")
+    if numero == "Alice":
+        print('One for Alice, one for me.')
+    elif numero == "Bob":
+        print('One for Bob, one for me.')
+    else:
+        print('One for you, one for me.')
